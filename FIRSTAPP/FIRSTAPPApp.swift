843feca7f4546a4a5ac2a9bdcf81303b37bd136e
2019imesh <2019imesh@gmail.com>
2023-02-28 20:08:31 +0530

    Initial Commit

---
+++ FIRSTAPP/FIRSTAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  FIRSTAPPApp.swift
+//  FIRSTAPP
+//
+//  Created by Chathura Jayasekara on 2023-02-28.
+//
+
+import SwiftUI
+
+@main
+struct FIRSTAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
